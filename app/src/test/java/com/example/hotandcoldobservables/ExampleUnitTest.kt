2ad08cf80e08c3133e86643f56fa4491075d0b70
jo <joseph.gayed@vodafone.com>
2019-07-21 21:52:36 +0200

    create simple cold ovservable that emmits 3 strings with just operator

--- app/src/test/java/com/example/hotandcoldobservables/ExampleUnitTest.kt
+++ app/src/test/java/com/example/hotandcoldobservables/ExampleUnitTest.kt
@@ -1,17 +1,21 @@
 package com.example.hotandcoldobservables
 
+import io.reactivex.Observable
 import org.junit.Test
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
+
+    /** Cold Observables are those which emits all the items whenever we subscribe to them.
+    Meaning, if two observers are subscribed to the same observable at different time,
+    they will receive all the emissions from that observable.
+    Its like playing a recorded mp3 song from a device, no matter when we play (subscribe)
+    it we can hear the entire song all the time.*/
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun initColdObservable() {
+        var coldObservable: Observable<String> = Observable.just("value_1", "value_2", "value_3")
+        coldObservable.subscribe { s -> println("Observer_1: $s") }
+        coldObservable.subscribe { s -> println("Observer_2: $s") }
     }
 }
